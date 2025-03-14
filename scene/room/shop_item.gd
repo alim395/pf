ba04226b22c17aa73ca44608b30aa6262fc2b312
Ali <65877768+alim395@users.noreply.github.com>
2025-03-14 22:16:16 +0300

    Updated Shop Scene
    Added Dynamic Shop Item organization with HBoxContainer.
    
    Implemented signals with bind to display item properties on Panel
    
    TODO:
    
    Add ability to buy items  by clicking, and visual indicators for focus/hovering.

--- scene/room/shop_item.gd
+++ scene/room/shop_item.gd
@@ -1,4 +1,4 @@
-extends Resource
+extends Control
 
 class_name ShopItem
 
@@ -6,7 +6,7 @@ class_name ShopItem
 var DEFAULT_TEXTURE = load("res://scene/shop/shop_items/UnknownItem.svg")
 
 # InventoryItem variables
-@export var name: String = ""
+@export var itemName: String = ""
 @export var texture: Texture2D
 @export var stackable: bool
 @export var stack_amount: int
@@ -20,15 +20,18 @@ enum {INSTANT,CONSUMABLE,WEAPON}
 
 # Constructor Function
 func _init(p_name = "Unnamed", p_texture = DEFAULT_TEXTURE, p_stackable = false, p_stack_amount = 16, p_price = 1, p_itemType = INSTANT, p_itemDesc = "Unknown Descritpion."):
-	name = p_name
+	itemName = p_name
 	texture = p_texture
 	stackable = p_stackable
-	stack_amount = p_stack_amount
+	if(stackable):
+		stack_amount = p_stack_amount
+	else:
+		stack_amount = 1
 	price = p_price
 	itemType = p_itemType
 	itemDesc = p_itemDesc
 
 # Item Conversion
 func ShopToInv(p_texture = DEFAULT_TEXTURE):
-	var InvItem = InventoryItem.new(self.name,p_texture,self.stackable,self.stack_amount)
+	var InvItem = InventoryItem.new(self.itemName,p_texture,self.stackable,self.stack_amount)
 	return InvItem
--- scene/shop/shop_ui.gd
+++ scene/shop/shop_ui.gd
@@ -7,7 +7,17 @@ var currPlayerMoney = 0
 @export var ItemType : RichTextLabel
 @export var ItemDesc : RichTextLabel
 @export var PlayerMoney: RichTextLabel
+@export var ShopItemContainer: Container
 
 func _ready() -> void:
 	currPlayerMoney = Global.PlayerMoney
 	PlayerMoney.text = str(currPlayerMoney) + " ₽"
+	var shopItems: Array = ShopItemContainer.get_children()
+	for item in shopItems:
+		item.focus_entered.connect(shopItemUpdate.bind(item.itemName,item.itemType, item.itemDesc))
+		item.mouse_entered.connect(shopItemUpdate.bind(item.itemName,item.itemType, item.itemDesc))
+
+func shopItemUpdate(iName, iType, iDesc):
+	ItemName.text = iName
+	ItemType.text = str(iType)
+	ItemDesc.text = iDesc
