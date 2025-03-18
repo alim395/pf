9b986b27ed4d86464b3a9961901d2aef7d008c9d
Ali <65877768+alim395@users.noreply.github.com>
2025-03-18 20:35:27 +0300

    Updated Shop Scene and Level Select
    Added Exit button to Shop Scene.
    Added DebugExit to Shop Scene.
    Added Shop Scene to Level Select.

--- scene/shop/shop.gd
+++ scene/shop/shop.gd
@@ -6,6 +6,10 @@ var currPlayerInventory = Global.PlayerInventory
 func _ready() -> void:
 	currPlayerMoney = Global.PlayerMoney
 
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("DebugExit"):
+		get_tree().change_scene_to_file("res://scene/menus/main_menu/main_menu.tscn")
+
 func buy_item(item:ShopItem):
 	if currPlayerMoney >= item.price:
 		currPlayerMoney -= item.price
@@ -13,3 +17,7 @@ func buy_item(item:ShopItem):
 		currPlayerInventory.insert(invItem)
 	else:
 		print("Not enough Rubles.")
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/menus/main_menu/main_menu.tscn")
