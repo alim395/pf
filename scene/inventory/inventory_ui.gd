7adeabbdfe0ac0a9f8e7ec7abd1b2eda592d361e
Ali <65877768+alim395@users.noreply.github.com>
2025-03-29 01:29:40 +0300

    Update inventory_ui.gd

--- scene/inventory/inventory_ui.gd
+++ scene/inventory/inventory_ui.gd
@@ -14,7 +14,7 @@ func update_slots():
 	for i in range(min(inventory.slots.size(), slots.size())):
 		slots[i].update(inventory.slots[i])
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Inventory"):
 		if is_open:
 			close()
